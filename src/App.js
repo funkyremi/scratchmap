d213da539754ecd067acebf26fcf380e87d6ffca
Rémi Testa <remi.testa@gmail.com>
2019-01-03 14:42:08 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    // eslint-disable-next-line no-undef
+    jQuery("#vmap").vectorMap({
+      map: "world_en",
+      enableZoom: false,
+    });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="vmap" style={{ width: "100%", height: window.innerHeight }} />
       </div>
     );
   }
